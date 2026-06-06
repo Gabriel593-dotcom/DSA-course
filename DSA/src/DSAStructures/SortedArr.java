c71d9da5e91f76169e7b002a06ff6c58c9646d8a
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2026-06-06 14:16:24 -0300

    feat - asymptotic analysis in SorteArr algorithms

--- DSA/src/DSAStructures/SortedArr.java
+++ DSA/src/DSAStructures/SortedArr.java
@@ -18,18 +18,22 @@ public class SortedArr {
         return new SortedArr(len);
     }
 
+    //O(1 + N) = O(N)
     public void add(int value) {
+        //O(1)
         if (arr[length - 1] != 0) {
             System.out.println("array is fully charged.");
             return;
         }
 
+        //O(1)
         if (arr[0] == 0) {
             arr[0] = value;
             verifyIfItsDuplicatedAndSetMinIndex(value, 0);
             return;
         }
 
+        //O(N)
         for (int i = length - 1; i > 0; i--) {
             if (arr[i - 1] == 0) continue;
 
@@ -48,18 +52,24 @@ public class SortedArr {
         }
     }
 
+    //O(LOG(N) + 1 + N-I) = O(LOG(N) + N) = O(N)
     public void delete(int value) {
+        //LOG(N)
         int index = binarySearch(value);
+
+        //O(1)
         if (index == -1) {
             System.out.println("value not found.");
             return;
         }
 
+        //0(1)
         if (index == length - 1) {
             arr[index] = 0;
             return;
         }
 
+        //O(N-I)
         for (int i = index; i < length; i++) {
             if (i == length - 1) {
                 arr[i] = 0;
@@ -70,6 +80,7 @@ public class SortedArr {
 
     }
 
+    //O(N)
     public void print() {
         for (int i = 0; i < length; i++) {
             System.out.print((length - 1 == i) ? arr[i] : arr[i] + ", ");
@@ -78,6 +89,8 @@ public class SortedArr {
         System.out.println();
     }
 
+
+    //O(LOG(N))
     public int binarySearch(int target) {
         int left = 0;
         int right = length - 1;
@@ -100,6 +113,7 @@ public class SortedArr {
         return -1;
     }
 
+    //O(1)
     private void verifyIfItsDuplicatedAndSetMinIndex(int value, int index) {
         if (frequencyFirstOccurrency.containsKey(value) && frequencyFirstOccurrency.get(value) < index) return;
 
